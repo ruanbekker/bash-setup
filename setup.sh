5d05709c785b59a4ba286b8ad0c0c9a24007f1a7
Ruan Bekker <ruan.ru.bekker@gmail.com>
2023-10-27 00:28:23 +0200

    add vimplug

--- setup.sh
+++ setup.sh
@@ -11,5 +11,8 @@ curl --silent https://raw.githubusercontent.com/ruanbekker/bash-setup/main/.vimr
 # Get vim colors
 curl --silent https://raw.githubusercontent.com/ruanbekker/bash-setup/main/.vim/colors/molokai.vim --create-dirs --output ~/.vim/colors/molokai.vim
 
+# Get vimplug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Set custom aliases
 curl --silent https://raw.githubusercontent.com/ruanbekker/bash-setup/main/.custom/aliases.sh --create-dirs --output ~/.custom/aliases.sh
