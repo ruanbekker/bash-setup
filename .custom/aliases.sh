62e7232adf8c444f1d8d556fbeba37124d5966ad
Ruan Bekker <ruan.ru.bekker@gmail.com>
2023-10-27 08:23:48 +0200

    Add aliases

--- .custom/aliases.sh
+++ .custom/aliases.sh
@@ -1,4 +1,11 @@
+# functions
 parse_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
+
+# aliases
+alias k=kubectl
+alias kns=kubens
+
+# shell
 export PS1="\[\e[36m\]\u@\h \[\e[32m\]\w \[\e[91m\]\$(parse_git_branch)\[\e[00m\] $ "
